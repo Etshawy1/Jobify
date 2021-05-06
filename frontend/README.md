3cb4c801f99aaa197de79fb347b1491790ca45d6
Moodrammer <mahmou.m.gody@gmail.com>
2021-05-06 17:25:00 +0200

    Add: rules to the SignUpForm component

--- frontend/README.md
+++ frontend/README.md
@@ -20,5 +20,13 @@ npm run build
 npm run lint
 ```
 
+### Notes for contribution
+- All vue file names are written in **PascalCase** "Ex. SignUp".
+- vue files are divided inside view and components folders into folders according to their functionality.
+- Use the convention for component tags in templates -> seperated by "-" (Ex. \<sign-up-form> for the **SignUpForm** component)
+- javaScript follows the **camelCase** naming convention as usual.
+- Give the routers "name" properties the same name as the component itself.
+
+
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- frontend/src/components/UserManagement/SignUpForm.vue
+++ frontend/src/components/UserManagement/SignUpForm.vue
@@ -1,12 +1,18 @@
 <template>
   <v-sheet color="white" rounded="lg">
-    <v-form v-model="form.valid" @submit.prevent="onSubmit">
+    <v-form v-model="formData.valid" @submit.prevent="onSubmit">
       <v-container>
         <div class="signupTitle">Sign Up and Start Applying For Jobs</div>
         <v-row justify="center" justify-md="center">
-          <!-- <v-spacer></v-spacer> -->
           <v-col cols="10" md="5" align-self="auto">
-            <v-text-field rounded-md outlined label="First name" dense>
+            <v-text-field
+              rounded-md
+              outlined
+              label="First name"
+              dense
+              v-model="formData.firstName"
+              :rules="[required('first name')]"
+            >
             </v-text-field>
           </v-col>
           <v-col cols="10" md="5" align-self="auto">
@@ -15,9 +21,10 @@
               outlined
               label="Last name"
               dense
+              v-model="formData.lastName"
+              :rules="[required('last name')]"
             ></v-text-field>
           </v-col>
-          <!-- <v-spacer></v-spacer> -->
         </v-row>
 
         <v-row justify="center">
@@ -28,6 +35,8 @@
               label="Email"
               dense
               type="email"
+              v-model="formData.email"
+              :rules="[required('email'), checkEmail()]"
             ></v-text-field>
           </v-col>
           <v-col cols="10">
@@ -37,15 +46,23 @@
               label="Password"
               dense
               type="password"
+              v-model="formData.password"
+              :rules="[required('password'), checkLength('password', 8)]"
             ></v-text-field>
           </v-col>
         </v-row>
 
         <v-row justify="center">
           <v-col cols="10">
-            <v-btn block dark color="rgba(0, 0, 255, 0.7)" type="submit" large
-              >submit</v-btn
-            >
+            <v-btn
+              block
+              color="blue darken-4"
+              type="submit"
+              large
+              class="white--text"
+              :disabled="!formData.valid"
+              >submit
+            </v-btn>
           </v-col>
         </v-row>
       </v-container>
@@ -57,8 +74,25 @@
 export default {
   data() {
     return {
-      form: {
+      formData: {
         valid: false,
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      },
+      required(propertyType) {
+        return (v) =>
+          (v && v.length > 0) || `Please enter Your ${propertyType}`;
+      },
+      checkEmail() {
+        return (v) =>
+          (v && /.+@.+\..+/.test(v)) || "Please enter a valid email address !";
+      },
+      checkLength(propertyType, minLength) {
+        return (v) =>
+          (v && v.length >= minLength) ||
+          `${propertyType} must be longer than ${minLength} characters`;
       },
     };
   },
